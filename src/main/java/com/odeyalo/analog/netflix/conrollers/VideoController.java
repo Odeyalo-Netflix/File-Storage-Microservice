50110c2f0057ec3cd8b9c2d295c8d4b21e72d2cb
Odeyalo <theprogrammerinfuture@gmail.com>
2022-06-12 12:24:11 +0300

    Removed unused imports

--- src/main/java/com/odeyalo/analog/netflix/conrollers/VideoController.java
+++ src/main/java/com/odeyalo/analog/netflix/conrollers/VideoController.java
@@ -6,14 +6,12 @@ import com.odeyalo.analog.netflix.exceptions.VideoUploadException;
 import com.odeyalo.analog.netflix.service.video.VideoStreamingService;
 import com.odeyalo.analog.netflix.service.video.VideoUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.ResourceRegion;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @RestController
@@ -32,6 +30,7 @@ public class VideoController {
     public ResponseEntity<ResourceRegion> streamVideo(@RequestParam String videoId, @RequestHeader(name = "range", required = false) String range) throws VideoNotFoundException, IOException {
         return this.streamService.stream(videoId, range);
     }
+
     @PostMapping("/upload")
     public ResponseEntity<?> saveVideo(@RequestParam String videoId, @RequestPart(name = "video") MultipartFile file) throws VideoUploadException, VideoAlreadyExistException {
         this.videoUploadService.uploadVideo(file, videoId);
