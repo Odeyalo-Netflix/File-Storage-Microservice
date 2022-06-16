f269b60888b16f49c12289c1eb77d4bed613dae9
Odeyalo <theprogrammerinfuture@gmail.com>
2022-06-16 22:40:25 +0300

    Updated

--- .github/entrypoint.sh
+++ .github/entrypoint.sh
@@ -2,7 +2,13 @@
 echo "Remove all containers"
 #docker stop $(docker ps -a -q)
 #docker rm $(docker ps -a -q)
-
+echo "Setting up folders..."
+mkdir -p /odeyalo/dev/microservices/filestorage/videos/
 echo "Building Dockerfile"
 
-docker image build -t "filestorage"
+docker build -t "filestorage" ./../
+
+
+echo "Starting docker-compose file"
+
+docker-compose run -f docker-compose.test.yml
