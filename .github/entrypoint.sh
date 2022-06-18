cf9ac82e831fc574bf344a463a1901b7a6c7269e
Odeyalo <theprogrammerinfuture@gmail.com>
2022-06-18 11:19:08 +0300

    Updated

--- .github/entrypoint.sh
+++ .github/entrypoint.sh
@@ -5,6 +5,7 @@ echo "Remove all containers"
 echo "Setting up folders..."
 mkdir -p /odeyalo/dev/microservices/filestorage/videos/
 echo "Building Dockerfile"
-
-docker build -t file-storage-test-image ./...
+cd ../../
+ls
+docker build -t ./github/file-storage-test-image .
 
