bb140f111f0582e73396d84de1fff2bda3e6302e
Odeyalo <theprogrammerinfuture@gmail.com>
2022-06-27 21:19:13 +0300

    Changed method return type, added docs

--- src/main/java/com/odeyalo/analog/netflix/service/video/VideoUploadService.java
+++ src/main/java/com/odeyalo/analog/netflix/service/video/VideoUploadService.java
@@ -5,7 +5,14 @@ import com.odeyalo.analog.netflix.exceptions.VideoUploadException;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface VideoUploadService {
-
-    void uploadVideo(MultipartFile video, String videoId) throws VideoUploadException, VideoAlreadyExistException;
+    /**
+     *
+     * @param video - video to save
+     * @param videoId - unique video id from other microservice
+     * @return - video url or path to streaming
+     * @throws VideoUploadException - if some problems was occurred while video upload
+     * @throws VideoAlreadyExistException - if video exist
+     */
+    String uploadVideo(MultipartFile video, String videoId) throws VideoUploadException, VideoAlreadyExistException;
 
 }
