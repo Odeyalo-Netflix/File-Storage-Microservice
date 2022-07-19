987755318a63cbd637fdc488cc5767a2fa555c54
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-19 18:38:16 +0300

    Added EnableEurekaClient annotation

--- src/main/java/com/odeyalo/analog/netflix/StorageApplication.java
+++ src/main/java/com/odeyalo/analog/netflix/StorageApplication.java
@@ -5,15 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class StorageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StorageApplication.class, args);
-        Logger logger = LoggerFactory.getLogger(StorageApplication.class);
     }
 }
 
