a8c09a1909cf25c90eb43aa994788ae4b95bbf04
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-19 18:41:17 +0300

    Added security config.

---
+++ src/main/java/com/odeyalo/analog/netflix/config/WebSecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.odeyalo.analog.netflix.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll();
+    }
+}
