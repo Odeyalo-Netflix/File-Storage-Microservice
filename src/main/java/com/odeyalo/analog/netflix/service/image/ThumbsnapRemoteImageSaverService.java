b6c14acbf57bc55891c0d0cca49bac37426691e2
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-29 19:04:59 +0300

    Saves image to remote server(Thumbsnap API)

--- src/main/java/com/odeyalo/analog/netflix/service/image/ThumbsnapRemoteImageSaverService.java
+++ src/main/java/com/odeyalo/analog/netflix/service/image/ThumbsnapRemoteImageSaverService.java
@@ -4,7 +4,9 @@ import com.odeyalo.analog.netflix.entity.Image;
 import com.odeyalo.analog.netflix.entity.ImageStorageType;
 import com.odeyalo.analog.netflix.exceptions.UploadException;
 import com.odeyalo.analog.netflix.repository.ImageRepository;
-import com.odeyalo.analog.netflix.service.storage.FileStorage;
+import com.odeyalo.analog.netflix.service.storage.FileUploader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -14,35 +16,28 @@ import java.io.IOException;
 
 @Service
 public class ThumbsnapRemoteImageSaverService implements ImageSaverService {
-    private final FileStorage fileStorage;
+    private final FileUploader fileUploader;
     private final ImageRepository imageRepository;
-
+    private final Logger logger = LoggerFactory.getLogger(ThumbsnapRemoteImageSaverService.class);
     @Autowired
-    public ThumbsnapRemoteImageSaverService(@Qualifier("thumbsnapSiteRemoteDiskFileStorage") FileStorage fileStorage, ImageRepository imageRepository) {
-        this.fileStorage = fileStorage;
+    public ThumbsnapRemoteImageSaverService(@Qualifier("thumbsnapSiteRemoteDiskFileUploader") FileUploader fileUploader, ImageRepository imageRepository) {
+        this.fileUploader = fileUploader;
         this.imageRepository = imageRepository;
     }
 
     @Override
-    public void saveUserImage(MultipartFile file, String userId) throws UploadException, IOException {
-        String path = this.fileStorage.save(file);
-        Image image = Image.builder()
-                .userId(userId)
-                .path(path)
-                .type(ImageStorageType.REMOTE)
-                .build();
-        this.imageRepository.save(image);
-    }
-
-    @Override
-    public String saveVideoImage(MultipartFile file, String remoteImageId) throws UploadException, IOException {
-        String path = this.fileStorage.save(file);
-        Image image = Image.builder()
-                .remoteImageId(remoteImageId)
-                .type(ImageStorageType.REMOTE)
-                .path(path)
-                .build();
-        this.imageRepository.save(image);
-        return path;
+    public Image saveImage(MultipartFile file) throws UploadException {
+        try {
+            String path = this.fileUploader.save(file);
+            Image image = this.imageRepository.save(Image.builder()
+                    .path(path)
+                    .type(ImageStorageType.REMOTE)
+                    .build());
+            this.logger.info("Saved image {}", image);
+            return image;
+        } catch (IOException ex) {
+            this.logger.error("File upload: {} has been failed.", file.getOriginalFilename(), ex);
+            throw new UploadException(String.format("Cannot upload file %s, please try again later", file.getName()));
+        }
     }
 }
