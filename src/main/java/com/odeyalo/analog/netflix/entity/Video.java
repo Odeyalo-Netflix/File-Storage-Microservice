1ee9937ef399e468d320184116d54dcb257a66c5
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-29 19:06:30 +0300

    Removed unused fields

--- src/main/java/com/odeyalo/analog/netflix/entity/Video.java
+++ src/main/java/com/odeyalo/analog/netflix/entity/Video.java
@@ -13,7 +13,6 @@ public class Video {
     @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
     private String id;
     @Column(unique = true, nullable = false, updatable = false)
-    private String videoId; //video id from other microservice
     private String path;
 
     public String getId() {
@@ -22,14 +21,6 @@ public class Video {
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    public String getVideoId() {
-        return videoId;
-    }
-
-    public void setVideoId(String videoId) {
-        this.videoId = videoId;
     }
 
     public String getPath() {
@@ -49,37 +40,30 @@ public class Video {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Video video = (Video) o;
-        return Objects.equals(id, video.id) && Objects.equals(videoId, video.videoId) && Objects.equals(path, video.path);
+        return Objects.equals(id, video.id) && Objects.equals(path, video.path);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, videoId, path);
+        return Objects.hash(id, path);
     }
 
     @Override
     public String toString() {
         return "Video{" +
                 "id='" + id + '\'' +
-                ", videoId='" + videoId + '\'' +
                 ", path='" + path + '\'' +
                 '}';
     }
 
     public static final class VideoBuilder {
         private String id;
-        private String videoId;
         private String path;
 
         private VideoBuilder() {}
 
         public VideoBuilder id(String id) {
             this.id = id;
-            return this;
-        }
-
-        public VideoBuilder videoId(String videoId) {
-            this.videoId = videoId;
             return this;
         }
 
@@ -91,7 +75,6 @@ public class Video {
         public Video build() {
             Video video = new Video();
             video.setId(id);
-            video.setVideoId(videoId);
             video.setPath(path);
             return video;
         }
