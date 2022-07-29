e3cf4d2534884334fd1548d285980d82892b30c6
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-29 19:03:32 +0300

    Added qualifier, changed interface implementation to class extends

--- src/main/java/com/odeyalo/analog/netflix/service/RemoteImageManager.java
+++ src/main/java/com/odeyalo/analog/netflix/service/RemoteImageManager.java
@@ -8,31 +8,22 @@ import com.odeyalo.analog.netflix.exceptions.UploadException;
 import com.odeyalo.analog.netflix.repository.ImageRepository;
 import com.odeyalo.analog.netflix.service.image.ImageSaverService;
 import com.odeyalo.analog.netflix.service.size.ImageResizer;
-import org.apache.commons.io.FilenameUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ByteArrayResource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
 
-@Component
-public class RemoteImageManager implements ImageManager {
+@Service
+public class RemoteImageManager extends AbstractImageManager {
     private final ImageRepository imageRepository;
-    private final Logger logger = LoggerFactory.getLogger(RemoteImageManager.class);
     private final ImageSaverService imageSaverService;
     private final ImageResizer imageResizer;
 
-    public RemoteImageManager(ImageRepository imageRepository, ImageSaverService imageSaverService, ImageResizer imageResizer) {
+    public RemoteImageManager(ImageRepository imageRepository, @Qualifier("thumbsnapRemoteImageSaverService") ImageSaverService imageSaverService, ImageResizer imageResizer) {
         this.imageRepository = imageRepository;
         this.imageSaverService = imageSaverService;
         this.imageResizer = imageResizer;
@@ -40,15 +31,10 @@ public class RemoteImageManager implements ImageManager {
 
 
     @Override
-    public void saveUserImage(MultipartFile file, String userId) throws IOException, UploadException {
-        this.logger.info("Saving image with name: {}. User id: {}", file.getOriginalFilename(), userId);
-        this.imageSaverService.saveUserImage(file, userId);
-        this.logger.info("Saved image with name: {}", file.getOriginalFilename());
-    }
-
-    @Override
-    public String saveVideoImage(MultipartFile file, String remoteImageId) throws UploadException, IOException {
-        return this.imageSaverService.saveVideoImage(file, remoteImageId);
+    public String saveImage(MultipartFile file) throws IOException, UploadException {
+        Image image = this.imageSaverService.saveImage(file);
+        this.logger.info("Saved image: {}", image);
+        return image.getId();
     }
 
     @Override
@@ -60,15 +46,8 @@ public class RemoteImageManager implements ImageManager {
             return new UrlResource(image.getPath());
         }
         this.logger.info("Reading image from path: {}", image.getPath());
-        BufferedImage bufferedImage = getBufferedImage(image);
+        BufferedImage bufferedImage = imageToBufferedImage(image);
         return bufferedImageToResource(bufferedImage, image);
-    }
-
-    @Override
-    public List<String> getImagesByUserId(String userId) {
-        List<Image> images = this.imageRepository.findAllByUserId(userId);
-        return images.stream().map(Image::getId)
-                .collect(Collectors.toList());
     }
 
     @Override
@@ -76,42 +55,13 @@ public class RemoteImageManager implements ImageManager {
         Image image = this.imageRepository.findById(imageId).orElseThrow(() -> {
             throw new ImageNotFoundException("Image not exist");
         });
-        BufferedImage bufferedImage = this.getBufferedImage(image);
+        BufferedImage bufferedImage = this.imageToBufferedImage(image);
         BufferedImage resizedImage = this.imageResizer.resize(bufferedImage, width, height);
         return bufferedImageToResource(resizedImage, image);
     }
 
-    @Override
-    public Resource getImageByRemoteImageId(String remoteImageId) throws IOException, ImageNotReadableException {
-        Image image = this.imageRepository.findImageByRemoteImageId(remoteImageId).orElseThrow(ImageNotFoundException::new);
-        if (image.getType() == ImageStorageType.REMOTE) {
-            return new UrlResource(image.getPath());
-        }
-        this.logger.info("Reading image from path: {}", image.getPath());
-        BufferedImage bufferedImage = getBufferedImage(image);
-        return bufferedImageToResource(bufferedImage, image);
-    }
 
     @Override
     public void compressImage(Image image) {
-    }
-
-    protected Resource bufferedImageToResource(BufferedImage bufferedImage, Image image) throws IOException {
-        ByteArrayOutputStream array = new ByteArrayOutputStream();
-        String extension = FilenameUtils.getExtension(image.getPath());
-        ImageIO.write(bufferedImage, extension, array);
-        byte[] bytes = array.toByteArray();
-        return new ByteArrayResource(bytes);
-    }
-
-    protected BufferedImage getBufferedImage(Image image) throws ImageNotReadableException {
-        String path = image.getPath();
-        File file = new File(path);
-        try {
-            return ImageIO.read(file);
-        } catch (IOException exception) {
-            this.logger.trace("Stacktrace: ", exception);
-            throw new ImageNotReadableException("Image cannot be read.");
-        }
     }
 }
