cb056eec124cb0ac589e892b1c2fe70d09f27516
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-29 18:58:44 +0300

    Now save file and returns Image entity

--- src/main/java/com/odeyalo/analog/netflix/service/image/ImageSaverService.java
+++ src/main/java/com/odeyalo/analog/netflix/service/image/ImageSaverService.java
@@ -1,13 +1,15 @@
 package com.odeyalo.analog.netflix.service.image;
 
+import com.odeyalo.analog.netflix.entity.Image;
 import com.odeyalo.analog.netflix.exceptions.UploadException;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 public interface ImageSaverService {
-
-    void saveUserImage(MultipartFile file, String userId) throws UploadException, IOException;
-
-    String saveVideoImage(MultipartFile file, String remoteImageId) throws UploadException, IOException;
+    /**
+     *
+     * @param file - image to save
+     * @return - returns saved image object
+     * @throws UploadException - if image cannot be uploaded
+     */
+    Image saveImage(MultipartFile file) throws UploadException;
 }
