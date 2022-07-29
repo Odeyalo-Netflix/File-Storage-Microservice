19b89aa63eb0d8d20c507ac22b3878eb5c7c2248
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-29 19:10:01 +0300

    Added security config

--- src/main/java/com/odeyalo/analog/netflix/config/WebSecurityConfiguration.java
+++ src/main/java/com/odeyalo/analog/netflix/config/WebSecurityConfiguration.java
@@ -17,6 +17,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll();
+        http.csrf().disable().httpBasic().and().authorizeRequests().anyRequest().permitAll();
     }
 }
