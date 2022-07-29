6921d3f5d1bdaf1ee7711e13b718c2befaeceaa0
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-29 18:58:11 +0300

    Removed custom queries

--- src/main/java/com/odeyalo/analog/netflix/repository/ImageRepository.java
+++ src/main/java/com/odeyalo/analog/netflix/repository/ImageRepository.java
@@ -4,13 +4,6 @@ import com.odeyalo.analog.netflix.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface ImageRepository extends JpaRepository<Image, String> {
-
-    List<Image> findAllByUserId(String userId);
-
-    Optional<Image> findImageByRemoteImageId(String remoteImageId);
 }
