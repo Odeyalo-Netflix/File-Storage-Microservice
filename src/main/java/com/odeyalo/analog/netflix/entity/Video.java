c055d4ae5566ca3b9c719dbd17627a8665e26fa7
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-14 15:06:54 +0300

    Removed boilerplate code with lombok

--- src/main/java/com/odeyalo/analog/netflix/entity/Video.java
+++ src/main/java/com/odeyalo/analog/netflix/entity/Video.java
@@ -1,82 +1,16 @@
 package com.odeyalo.analog.netflix.entity;
 
-import org.hibernate.annotations.GenericGenerator;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
 @Table(name = "videos")
-public class Video {
-    @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
-    private String id;
-    @Column(unique = true, nullable = false, updatable = false)
-    private String path;
+@Inheritance(strategy = InheritanceType.JOINED)
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Video extends GenericFileEntity {
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public static VideoBuilder builder() {
-        return new VideoBuilder();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Video video = (Video) o;
-        return Objects.equals(id, video.id) && Objects.equals(path, video.path);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, path);
-    }
-
-    @Override
-    public String toString() {
-        return "Video{" +
-                "id='" + id + '\'' +
-                ", path='" + path + '\'' +
-                '}';
-    }
-
-    public static final class VideoBuilder {
-        private String id;
-        private String path;
-
-        private VideoBuilder() {}
-
-        public VideoBuilder id(String id) {
-            this.id = id;
-            return this;
-        }
-
-        public VideoBuilder path(String path) {
-            this.path = path;
-            return this;
-        }
-
-        public Video build() {
-            Video video = new Video();
-            video.setId(id);
-            video.setPath(path);
-            return video;
-        }
-    }
 }
