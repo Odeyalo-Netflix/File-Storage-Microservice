9cb6040bcaf39ab7a27c303a4906c41e0675fe5b
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-14 15:05:43 +0300

    Added endpoints to delete and get information about video, changed dto's imports

--- src/main/java/com/odeyalo/analog/netflix/conrollers/VideoController.java
+++ src/main/java/com/odeyalo/analog/netflix/conrollers/VideoController.java
@@ -4,10 +4,18 @@ import com.odeyalo.analog.netflix.annotation.ValidVideoFIle;
 import com.odeyalo.analog.netflix.exceptions.VideoAlreadyExistException;
 import com.odeyalo.analog.netflix.exceptions.VideoNotFoundException;
 import com.odeyalo.analog.netflix.exceptions.VideoUploadException;
+import com.odeyalo.analog.netflix.service.files.FileInformationService;
+import com.odeyalo.analog.netflix.service.video.VideoManager;
 import com.odeyalo.analog.netflix.service.video.VideoStreamingService;
-import com.odeyalo.analog.netflix.service.video.facade.KafkaEventPublisherVideoSaverServiceFacade;
+import com.odeyalo.support.clients.filestorage.dto.FileInformationResponseDTO;
+import com.odeyalo.support.clients.filestorage.dto.LinkResponse;
+import com.odeyalo.support.clients.filestorage.dto.SuccessUploadVideoResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.support.ResourceRegion;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -15,19 +23,22 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/video")
 @Validated
 public class VideoController {
-    private final KafkaEventPublisherVideoSaverServiceFacade videoSaverService;
+    private final VideoManager videoManager;
     private final VideoStreamingService<ResponseEntity<ResourceRegion>> streamService;
+    private final FileInformationService fileInformationService;
 
     @Autowired
-    public VideoController(KafkaEventPublisherVideoSaverServiceFacade videoSaverService, VideoStreamingService<ResponseEntity<ResourceRegion>> streamService) {
-        this.videoSaverService = videoSaverService;
+    public VideoController(VideoManager videoManager, VideoStreamingService<ResponseEntity<ResourceRegion>> streamService,
+                           @Qualifier("videoFileInformationService") FileInformationService fileInformationService) {
+        this.videoManager = videoManager;
         this.streamService = streamService;
+        this.fileInformationService = fileInformationService;
     }
 
     @GetMapping(value = "/watch", produces = "application/octet-stream")
@@ -35,12 +46,24 @@ public class VideoController {
         return this.streamService.stream(videoId, range);
     }
 
+    @GetMapping(value = "/info/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<FileInformationResponseDTO> streamVideo(@PathVariable String id) throws VideoNotFoundException, IOException {
+        FileInformationResponseDTO information = this.fileInformationService.getInformation(id);
+        return new ResponseEntity<>(information, HttpStatus.OK);
+    }
+
     @PostMapping("/upload")
     public ResponseEntity<?> saveVideo(@ValidVideoFIle @RequestPart(name = "video") MultipartFile file) throws VideoUploadException, VideoAlreadyExistException {
-        String id = this.videoSaverService.uploadVideo(file);
-        String s = MvcUriComponentsBuilder.fromMethodName(VideoController.class, "streamVideo", id, 0).build().toString();
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("videoId", s);
-        return ResponseEntity.ok(map);
+        String id = this.videoManager.saveVideo(file);
+        String url = MvcUriComponentsBuilder.fromMethodName(VideoController.class, "streamVideo", id, 0).build().toString();
+        SuccessUploadVideoResponseDTO dto = new SuccessUploadVideoResponseDTO(id,
+                Collections.singletonList(new LinkResponse(url, HttpMethod.GET.name())));
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping("/delete/{videoId}")
+    public ResponseEntity<?> deleteVideo(@PathVariable String videoId) {
+        this.videoManager.deleteVideo(videoId);
+        return ResponseEntity.status(204).build();
     }
 }
