41dd0d84de824e46226a80584f47ee159f344481
Odeyalo <theprogrammerinfuture@gmail.com>
2022-10-07 12:31:35 +0300

    Added getLink() method

--- src/main/java/com/odeyalo/analog/netflix/service/files/GenericFileInformationService.java
+++ src/main/java/com/odeyalo/analog/netflix/service/files/GenericFileInformationService.java
@@ -3,18 +3,30 @@ package com.odeyalo.analog.netflix.service.files;
 import com.odeyalo.analog.netflix.entity.GenericFileEntity;
 import com.odeyalo.analog.netflix.repository.GenericFileEntityRepository;
 import com.odeyalo.support.clients.filestorage.dto.FileInformationResponseDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.FileNotFoundException;
 
 @Service
 public class GenericFileInformationService implements FileInformationService {
-    private final GenericFileEntityRepository repository;
+    protected GenericFileEntityRepository repository;
+    protected String servletContextPath;
+    protected String host;
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    public GenericFileInformationService(GenericFileEntityRepository repository) {
+    public GenericFileInformationService(GenericFileEntityRepository repository,
+                                         @Value("${app.host}") String host,
+                                         @Value("#{servletContext.contextPath}") String servletContextPath) {
         this.repository = repository;
+        this.servletContextPath = servletContextPath;
+        this.host = host;
     }
 
 
@@ -33,5 +45,17 @@ public class GenericFileInformationService implements FileInformationService {
                 .size(fileEntity.getSize())
                 .type(fileEntity.getType())
                 .build();
+    }
+
+    /**
+     * Return http link to the file. Example: https://static.application.files/{contextPath}/{url}/{imageId}
+     *
+     * @param args - args id to create link
+     * @return - string with built url
+     */
+    protected String getLink(Class<?> controller, String methodName, Object... args) {
+        return MvcUriComponentsBuilder
+                .fromMethodName(UriComponentsBuilder.fromHttpUrl(host).path(servletContextPath), controller, methodName, args)
+                .toUriString();
     }
 }
