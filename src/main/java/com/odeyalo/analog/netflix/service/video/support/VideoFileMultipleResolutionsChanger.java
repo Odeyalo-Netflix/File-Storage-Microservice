a11915d15c0af304ea7012e732cf8a0c1728cd0b
Odeyalo <theprogrammerinfuture@gmail.com>
2022-10-07 12:01:53 +0300

    Change given video to multiple resolutions

---
+++ src/main/java/com/odeyalo/analog/netflix/service/video/support/VideoFileMultipleResolutionsChanger.java
@@ -0,0 +1,20 @@
+package com.odeyalo.analog.netflix.service.video.support;
+
+import com.odeyalo.analog.netflix.service.files.ResizedFileSavingResult;
+import com.odeyalo.analog.netflix.service.files.VideoResolution;
+
+import java.util.List;
+
+/**
+ * Change given video to multiple resolutions
+ */
+public interface VideoFileMultipleResolutionsChanger {
+
+    /**
+     * Convert given video file to multiple files with different resolutions
+     * @param pathToVideo - path to original video
+     * @param videoResolutions - required resolutions for given video,
+     * @return - list of ResizedFileSavingResult with info about path and other info.
+     */
+    List<ResizedFileSavingResult> changeVideoResolution(String pathToVideo, List<VideoResolution> videoResolutions);
+}
