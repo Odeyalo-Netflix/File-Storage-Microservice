93851894f02bc01d7e42974f7600576a22f2440e
Odeyalo <theprogrammerinfuture@gmail.com>
2022-10-07 12:22:42 +0300

    Publish event when video was saved

--- src/main/java/com/odeyalo/analog/netflix/service/video/facade/KafkaEventPublisherVideoSaverServiceFacade.java
+++ src/main/java/com/odeyalo/analog/netflix/service/video/facade/KafkaEventPublisherVideoSaverServiceFacade.java
@@ -1,49 +1,60 @@
 package com.odeyalo.analog.netflix.service.video.facade;
 
+import com.odeyalo.analog.netflix.entity.Video;
 import com.odeyalo.analog.netflix.exceptions.VideoAlreadyExistException;
 import com.odeyalo.analog.netflix.exceptions.VideoUploadException;
+import com.odeyalo.analog.netflix.repository.VideoRepository;
+import com.odeyalo.analog.netflix.service.broker.kafka.sender.KafkaMessageSender;
 import com.odeyalo.analog.netflix.service.video.VideoSaverService;
+import com.odeyalo.analog.netflix.support.events.EventPublisher;
+import com.odeyalo.analog.netflix.support.events.OriginalVideoFileSavedEvent;
 import com.odeyalo.support.clients.filestorage.dto.VideoUploadedSuccessMessageDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
-import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.concurrent.ExecutionException;
+import java.util.UUID;
 
 /**
  * Publish event to specific kafka topic if upload was successful or failed
  */
 @Component
 public class KafkaEventPublisherVideoSaverServiceFacade implements VideoSaverServiceFacade {
-    private final KafkaTemplate<String, VideoUploadedSuccessMessageDTO> template;
+    private final KafkaMessageSender<String, VideoUploadedSuccessMessageDTO> template;
     private final VideoSaverService videoSaverService;
     private final Logger logger = LoggerFactory.getLogger(KafkaEventPublisherVideoSaverServiceFacade.class);
+    private final EventPublisher eventPublisher;
+    private final VideoRepository repository;
 
     @Autowired
-    public KafkaEventPublisherVideoSaverServiceFacade(KafkaTemplate<String, VideoUploadedSuccessMessageDTO> template, VideoSaverService videoSaverService) {
+    public KafkaEventPublisherVideoSaverServiceFacade(KafkaMessageSender<String, VideoUploadedSuccessMessageDTO> template,
+                                                      VideoSaverService videoSaverService, EventPublisher eventPublisher,
+                                                      VideoRepository repository) {
         this.template = template;
         this.videoSaverService = videoSaverService;
+        this.eventPublisher = eventPublisher;
+        this.repository = repository;
     }
 
     @Override
     public String uploadVideo(MultipartFile file) throws VideoUploadException {
         try {
-            String fileId = this.videoSaverService.uploadVideo(file);
-            ListenableFuture<SendResult<String, VideoUploadedSuccessMessageDTO>> message = this.template.send("VIDEO_UPLOADED_SUCCESS",
-                    new VideoUploadedSuccessMessageDTO("EVENT_1", "VIDEO_ID_1", fileId));
-            this.logger.info("Successful uploaded video, kafka message dto: {}", message.get().toString());
-            return fileId;
+            String videoId = this.videoSaverService.uploadVideo(file);
+            Video video = repository.findById(videoId).get();
+            this.eventPublisher.publishEvent(OriginalVideoFileSavedEvent.EVENT_NAME, new OriginalVideoFileSavedEvent(videoId, video.getPath()));
+            this.template.send("VIDEO_UPLOADED_SUCCESS", new VideoUploadedSuccessMessageDTO(generateEventId(), video.getId(), videoId));
+            return videoId;
         } catch (VideoUploadException | VideoAlreadyExistException e) {
             this.logger.error("Failed to upload video", e);
             this.template.send("VIDEO_UPLOAD_FAILED", new VideoUploadedSuccessMessageDTO("", "", ""));
-        } catch (InterruptedException | ExecutionException e) {
-            this.logger.error("Failed to get message from kafka", e);
         }
         throw new VideoUploadException("Failed to upload video to server");
+    }
+
+
+    private String generateEventId() {
+        return UUID.randomUUID().toString();
     }
 }
