78a17fb42b73eef86f65e14f2d6b7912d6404ef9
Odeyalo <theprogrammerinfuture@gmail.com>
2022-10-07 12:04:35 +0300

    Now url building is not hardcoded and can be easy changed

--- src/main/java/com/odeyalo/analog/netflix/service/files/VideoFileInformationService.java
+++ src/main/java/com/odeyalo/analog/netflix/service/files/VideoFileInformationService.java
@@ -1,31 +1,55 @@
 package com.odeyalo.analog.netflix.service.files;
 
+import com.odeyalo.analog.netflix.conrollers.VideoController;
+import com.odeyalo.analog.netflix.entity.Video;
 import com.odeyalo.analog.netflix.repository.GenericFileEntityRepository;
+import com.odeyalo.analog.netflix.repository.VideoRepository;
 import com.odeyalo.support.clients.filestorage.dto.FileInformationResponseDTO;
 import com.odeyalo.support.clients.filestorage.dto.LinkResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.FileNotFoundException;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class VideoFileInformationService extends GenericFileInformationService {
+    private final VideoRepository videoRepository;
 
-    private final String host;
+    @Autowired
     public VideoFileInformationService(GenericFileEntityRepository repository,
-                                       @Value("${app.host}") String host) {
-        super(repository);
-        this.host = host;
+                                       @Value("${app.host}") String host,
+                                       @Value("#{servletContext.contextPath}") String servletContextPath, VideoRepository videoRepository) {
+        super(repository, host, servletContextPath);
+        this.videoRepository = videoRepository;
     }
 
     @Override
-    public FileInformationResponseDTO getInformation(String fileId) throws FileNotFoundException {
-        FileInformationResponseDTO videoInfo = super.getInformation(fileId);
-        String url = "/api/v1/video/watch?videoId=" + videoInfo.getFileId();
-        String link = host + url;
-        videoInfo.setUrl(url);
-        videoInfo.setLinks(Collections.singletonList(new LinkResponse(link, "GET")));
-        return videoInfo;
+    public VideoFileInformationResponseDTO getInformation(String fileId) throws FileNotFoundException {
+        FileInformationResponseDTO genericInfo = super.getInformation(fileId);
+        String url = buildGetVideoByIdUrl(fileId, servletContextPath, UriComponentsBuilder.newInstance());
+        String link = getLink(VideoController.class, "streamVideo", fileId, "");
+        genericInfo.setUrl(url);
+        genericInfo.setLinks(Collections.singletonList(new LinkResponse(link, "GET")));
+        Video video = videoRepository.findById(fileId).orElseThrow(FileNotFoundException::new);
+        return new VideoFileInformationResponseDTO(genericInfo, getResizedVideoFileResponseDTO(video));
+    }
+
+
+
+    List<ResizedVideoFileResponseDTO> getResizedVideoFileResponseDTO(Video video)  {
+        return video.getResizedVideos().stream().map(v -> new ResizedVideoFileResponseDTO(new VideoResolution(v.getWidth(), v.getHeight()),
+                Collections.singletonList(new LinkResponse(getLink(VideoController.class, "streamVideo", v.getId(), ""), "GET")))).collect(Collectors.toList());
+    }
+
+    protected String buildGetVideoByIdUrl(String videoId, String path, UriComponentsBuilder uriComponentsBuilder) {
+        return MvcUriComponentsBuilder
+                .fromMethodName(uriComponentsBuilder.path(path), VideoController.class, "streamVideo", videoId, "")
+                .toUriString();
     }
 }
