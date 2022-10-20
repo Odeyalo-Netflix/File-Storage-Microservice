92692645e0b0ff8d361b42baf0040d600e963898
Odeyalo <theprogrammerinfuture@gmail.com>
2022-10-20 19:45:02 +0300

    Removed unused code

--- src/main/java/com/odeyalo/analog/netflix/config/ApacheKafkaProducerConfiguration.java
+++ src/main/java/com/odeyalo/analog/netflix/config/ApacheKafkaProducerConfiguration.java
@@ -5,19 +5,13 @@ import com.odeyalo.analog.netflix.service.broker.kafka.sender.DefaultKafkaMessag
 import com.odeyalo.analog.netflix.service.broker.kafka.support.KafkaBrokerNotAvailableHandler;
 import com.odeyalo.analog.netflix.service.broker.kafka.support.LoggingKafkaBrokerNotAvailableHandler;
 import com.odeyalo.support.clients.filestorage.dto.VideoUploadedSuccessMessageDTO;
-import lombok.extern.log4j.Log4j2;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
-import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
-import org.springframework.kafka.support.ProducerListener;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
@@ -43,12 +37,7 @@ public class ApacheKafkaProducerConfiguration {
 
     @Bean
     public KafkaTemplate<String, VideoUploadedSuccessMessageDTO> videoUploadedSuccessMessageDTOKafkaTemplate(ProducerFactory<String, VideoUploadedSuccessMessageDTO> factory) {
-        KafkaTemplate<String, VideoUploadedSuccessMessageDTO> template = new KafkaTemplate<>(factory);
-        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(template, (r, e) -> {
-            System.out.println("Exception" + e);
-            return new TopicPartition(r.topic(), r.partition());
-        });
-        return template;
+        return new KafkaTemplate<>(factory);
     }
 
     @Bean
@@ -70,14 +59,5 @@ public class ApacheKafkaProducerConfiguration {
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         config.put(ProducerConfig.RECONNECT_BACKOFF_MS_CONFIG, 10000);
         return config;
-    }
-
-
-    @Log4j2
-    static class CustomProducerListener implements ProducerListener<String, VideoUploadedSuccessMessageDTO> {
-        @Override
-        public void onError(ProducerRecord<String, VideoUploadedSuccessMessageDTO> producerRecord, RecordMetadata recordMetadata, Exception exception) {
-            log.error("Error", exception);
-        }
     }
 }
